351159c4cefaaf75646ee0b6dffc51767494d96a
asare christian <asarechristian9090@gmail.com>
2026-09-11 22:20:36 +0000

    run latest migrations

---
+++ drizzle/0006_abnormal_mentallo.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "content_item" ADD COLUMN "source_url" text;--> statement-breakpoint
+ALTER TABLE "content_item" ADD COLUMN "source_author" text;--> statement-breakpoint
+ALTER TABLE "content_item" ADD COLUMN "source_license" text;
